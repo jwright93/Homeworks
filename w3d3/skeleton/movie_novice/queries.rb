6fe4934bc6b975ce4c2db840b349d1eddd13b531
Jonathon Wright <jonwright93@gmail.com>
2017-02-14 20:46:41 -0800

    add homework from beginning of week 4

--- w3d3/skeleton/movie_novice/queries.rb
+++ w3d3/skeleton/movie_novice/queries.rb
@@ -31,8 +31,6 @@ def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
   Movie.select(:id, :title).where('score >= 9')
-
-
 end
 
 def star_wars
@@ -40,9 +38,6 @@ def star_wars
   # hint: use 'select' and 'where'
 
     Movie.all.select(:id, :title, :yr).where("title LIKE '%Star Wars%'")
-
-
-
 end
 
 
---
+++ w4d2/AppName/db/migrate/20170215041604_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+    add_index :users, :username
+  end
+end
